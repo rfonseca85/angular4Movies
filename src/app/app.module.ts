1391531e6e2dad7217fc768d294bb19adca3d5bd
Fonseca <a622549@fmr.com>
2018-05-18 22:14:18 -0400

    creating first page to inclunde some bootstrap-grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BootstrapGridComponent } from './home/bootstrap-grid/bootstrap-grid.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        BootstrapGridComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/home/bootstrap-grid/bootstrap-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bootstrap-grid',
+  templateUrl: './bootstrap-grid.component.html',
+  styleUrls: ['./bootstrap-grid.component.scss']
+})
+export class BootstrapGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
