45cd8e946a747a20d12ac9e738204f01f1e8022f
rfonseca85 <rfonseca85@yahoo.ca>
2018-05-21 14:44:58 -0400

    Fixing heroku deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+	production: true,
+	localUrl: "http://localhost:3000",
+	realUrl: "--------------------------------------",
+	type: "dev",
+	envName: "dev"
 };
