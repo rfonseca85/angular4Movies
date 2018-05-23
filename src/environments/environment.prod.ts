e5f2c12dc5aaf784b50c983be8604213b737d1cd
rfonseca85 <rfonseca85@yahoo.ca>
2018-05-22 20:31:14 -0400

    Fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,3 @@
 export const environment = {
-	production: true,
-	localUrl: "http://localhost:3000",
-	realUrl: "--------------------------------------",
-	type: "dev",
-	envName: "dev"
+	production: false,
 };
